ec668cf5ff3d0a668bf713e5c20fe2a296693533
Tom Bloor <t.bloor@shadowcat.co.uk>
2017-04-19 15:57:02 +0100

    Added actual class and documentation for module, with a test

--- README.md
+++ README.md
@@ -1,14 +1,55 @@
 # NAME
 
-MooX::Options::Actions - Blah blah blah
+MooX::Options::Actions - Instant one class CLI App
 
 # SYNOPSIS
 
+In MyApp::Script:
+
+    package MyApp::Script;
+
     use MooX::Options::Actions;
+
+    option 'boo' => (
+      is => 'ro',
+      format => 's',
+      required => 1,
+      doc => "Surprise!"
+    );
+
+    sub cmd_dump {
+      my ( $self ) = @_;
+
+      print "Message: [" . $self->boo . "]\n";
+    }
+
+    1;
+
+In script.pl:
+
+    #! /usr/bin/env perl
+
+    use strict;
+    use warnings;
+
+    use MyApp::Script;
+
+    MyApp::Script->new_with_actions;
+
+On the command line:
+
+    $ ./script.pl dump --boo Hello
+    Message: [Hello]
 
 # DESCRIPTION
 
-MooX::Options::Actions is
+MooX::Options::Actions is a set of packages designed to make setting up and
+creating command line applications really easy. It automatically imports Moo,
+MooX::Options, namespace::clean, and a
+["new\_with\_actions" in MooX::Options::Actions::Builder](https://metacpan.org/pod/MooX::Options::Actions::Builder#new_with_actions) function to set up the top
+level commands. this means you only need to include this one module, and then
+you can set up options as from [MooX::Options](https://metacpan.org/pod/MooX::Options), and set up commands to act on
+those options by creating subroutines with the `cmd_` prefix.
 
 # AUTHOR
 
@@ -24,3 +65,5 @@ This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 # SEE ALSO
+
+[MooX::Options](https://metacpan.org/pod/MooX::Options) [Import::Into](https://metacpan.org/pod/Import::Into)
--- lib/MooX/Options/Actions.pm
+++ lib/MooX/Options/Actions.pm
@@ -1,25 +1,77 @@
 package MooX::Options::Actions;
 
 use strict;
-use 5.008_005;
-our $VERSION = '0.01';
+use warnings;
 
-1;
-__END__
+use Import::Into;
 
-=encoding utf-8
+our $VERSION = '0.001';
 
 =head1 NAME
 
-MooX::Options::Actions - Blah blah blah
+MooX::Options::Actions - Instant one class CLI App
 
 =head1 SYNOPSIS
 
+In MyApp::Script:
+  
+  package MyApp::Script;
+
   use MooX::Options::Actions;
+
+  option 'boo' => (
+    is => 'ro',
+    format => 's',
+    required => 1,
+    doc => "Surprise!"
+  );
+
+  sub cmd_dump {
+    my ( $self ) = @_;
+
+    print "Message: [" . $self->boo . "]\n";
+  }
+
+  1;
+
+In script.pl:
+
+  #! /usr/bin/env perl
+
+  use strict;
+  use warnings;
+
+  use MyApp::Script;
+
+  MyApp::Script->new_with_actions;
+
+On the command line:
+
+  $ ./script.pl dump --boo Hello
+  Message: [Hello]
 
 =head1 DESCRIPTION
 
-MooX::Options::Actions is
+MooX::Options::Actions is a set of packages designed to make setting up and
+creating command line applications really easy. It automatically imports Moo,
+MooX::Options, namespace::clean, and a
+L<MooX::Options::Actions::Builder/new_with_actions> function to set up the top
+level commands. this means you only need to include this one module, and then
+you can set up options as from L<MooX::Options>, and set up commands to act on
+those options by creating subroutines with the C<cmd_> prefix.
+
+=cut
+
+sub import {
+  my $target = caller;
+  Moo->import::into($target);
+  MooX::Options->import::into($target, protect_argv => 0 );
+  namespace::clean->import::into(
+    $target,
+    -except => [ qw/ _options_data _options_config / ],
+  );
+  MooX::Options::Actions::Builder->import::into($target, qw/ new_with_actions /);
+}
 
 =head1 AUTHOR
 
@@ -36,4 +88,8 @@ it under the same terms as Perl itself.
 
 =head1 SEE ALSO
 
+L<MooX::Options> L<Import::Into>
+
 =cut
+
+1;
